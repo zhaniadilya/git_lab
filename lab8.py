202d5340ae505c2e747a1ddcaaa9f6e04fff2187
zhaniadilya <zhaniadilya@gmail.com>
2023-04-08 16:03:19 +0600

    8 lab Komandnaya rabota

---
+++ lab8.py
@@ -0,0 +1,13 @@
+s = input('Vvedite stroku \n')
+flag=1
+string=''
+for i in range(len(s)):
+    if s[i]!='':
+        string+=s[i]
+print(string)
+for i in range(len(s)//2):
+    if string[i]!=string[-i-1]:
+        flag=0
+        break
+if flag: print('Palindrom')
+else: print('Ne palindrom')
